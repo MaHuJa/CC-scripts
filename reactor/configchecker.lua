390b0bcd35ebef4ed981f40eecbee343ba033d07
MaHuJa <github@mahuja.net>
2014-03-17 13:47:17 +0100

    Reactor identity check filled in

--- reactor/configchecker.lua
+++ reactor/configchecker.lua
@@ -1,7 +1,7 @@
 
 assert(reactorOff and reactorOff())
 assert(reactorOn)
-assert(reactor and reactor.inventoryName = "") -- todo
+assert(reactor and reactor.getInventoryName() == "Nuclear Reactor")
 assert(supplier and supplier.getInventorySize)
 assert(io.open(logfile,"a")):close()
 assert(reactor_min < reactor_max)
