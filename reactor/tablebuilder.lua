45654bf87c11388eb6e6273d8d16549eb9d17a38
MaHuJa <github@mahuja.net>
2014-03-17 09:22:54 +0100

    Trimmed reactortable
    I believe the name is the only thing I'll actually use.

--- reactor/tablebuilder.lua
+++ reactor/tablebuilder.lua
@@ -3,9 +3,13 @@ file = assert(io.open("reactortable","w"))
 reac = assert(peripheral.wrap(reactorname))
 
 reactor = reac.getAllStacks()
+out = {}
+local k,v
+for k,v in pairs(reactor) do
+	out[k] = v[name]
+end
 
+-- TODO: NCOM does not have serialize
 file:write(textutils.serialize(reactor))
 file:close()
-
-
 
