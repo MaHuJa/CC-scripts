217e6d8846e473c18ea1f8548a9766fef0f523b9
MaHuJa <github@mahuja.net>
2014-03-17 16:00:42 +0100

    Reactor program loader updated

--- reactor/reactor.lua
+++ reactor/reactor.lua
@@ -1,12 +1,21 @@
 -- This file provides the entrypoint for the program
+dofile "config"
+dofile "configchecker"
+
 
 if peripheral then -- CC
   dofile "logger_cc.lua"
-  dofile "config"
 else
-  component = assert(require "component")
   dofile "logger_ncom.lua"
 end
+logmessage ("Config seems ok.");
+sleep = sleep or os.sleep
+
+dofile "utilities.lua"
+dofile "componentcheck.lua"
+
+manualmaintenance = assert(loadfile("manualmaintenance.lua"))
+automaintain = assert(loadfile("automaintain.lua"))
 
 
 
