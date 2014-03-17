53fcaf8a586b36947f42f250980d052f63dba951
MaHuJa <github@mahuja.net>
2014-03-17 15:33:24 +0100

    addItem removeItem

---
+++ reactor/utilities.lua
@@ -0,0 +1,21 @@
+
+function addItem(name,index)
+	local slot
+	-- search through supplier inventory
+	local k,v
+	for k,v in pairs(supplier.getAllStacks()) do
+		if v.name == name then
+			slot = k
+			break
+		end
+	end
+	if not slot then 
+		manualmaintenance()	-- never returns
+		return addItem(name,index) -- but if it does, it means try again
+	end
+	supplier.pushItem(supplierside,slot,1,index)
+end
+
+function removeItem (index)
+	assert(supplier.pullItem(supplierside,index)==1)
+end
